dc11edf936afe8b509e21ece0715e73c34b78eb1
winnie tongle <winnie.carole.tongle.djoukeng@mni.thm.de>
2021-08-08 20:02:37 +0200

    add login.php und style.css

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<h1>Login Form</h1>
+<center>
+<div>
+<!--INSERT INTO `login` (`id`, `username`, `password`, `usertype`) VALUES (NULL, 'admin', 'winnie', 'user');-->
+    <form action="#" method="POST">
+
+        <div>
+            <label>username</label>
+            <input type="text" name="username" required>
+        </div>
+        <br><br>
+
+        <div>
+            <label>password</label>
+            <input type="password" name="password" required>
+        </div>
+        <br><br>
+
+        <div>
+
+            <input type="submit" value="Login">
+        </div>
+
+
+    </form>
+
+</div>
+</center
+</body>
+</html>
